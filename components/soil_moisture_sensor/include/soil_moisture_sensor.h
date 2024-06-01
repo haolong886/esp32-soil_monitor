146219fdf29fba8e0125493bb25e9693e580d364
haolong86 <haolong86@qq.com>
2024-06-02 04:48:53 +0800

    init

---
+++ components/soil_moisture_sensor/include/soil_moisture_sensor.h
@@ -0,0 +1,36 @@
+
+
+/*---------------------------------------------------------------
+        ADC General Macros
+---------------------------------------------------------------*/
+//ADC1 Channels
+#if CONFIG_IDF_TARGET_ESP32
+#define EXAMPLE_ADC1_CHAN0          CONFIG_PLANT_SOIL_MOISTURE_SENSOR_ADC1_CHAN
+//#define EXAMPLE_ADC1_CHAN1          ADC_CHANNEL_5
+#else
+#define EXAMPLE_ADC1_CHAN0          ADC_CHANNEL_2
+#define EXAMPLE_ADC1_CHAN1          ADC_CHANNEL_3
+#endif
+
+#if (SOC_ADC_PERIPH_NUM >= 2) && !CONFIG_IDF_TARGET_ESP32C3
+/**
+ * On ESP32C3, ADC2 is no longer supported, due to its HW limitation.
+ * Search for errata on espressif website for more details.
+ */
+//#define EXAMPLE_USE_ADC2            1
+#endif
+
+#if EXAMPLE_USE_ADC2
+//ADC2 Channels
+#if CONFIG_IDF_TARGET_ESP32
+#define EXAMPLE_ADC2_CHAN0          ADC_CHANNEL_0
+#else
+#define EXAMPLE_ADC2_CHAN0          ADC_CHANNEL_0
+#endif
+#endif  //#if EXAMPLE_USE_ADC2
+
+#define EXAMPLE_ADC_ATTEN           ADC_ATTEN_DB_12
+
+void soil_moisture_sensor_init(void);
+int soil_moisture_sensor_read(void);
+void soil_moisture_sensor_tear_down(void);
